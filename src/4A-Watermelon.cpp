080bb5824322ed5fc7111bbcae01c448daad204c
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-07 17:41:20 +0100

    solution 4A

---
+++ src/4A-Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int weight;
+    cin >> weight;
+    if (weight % 2 == 0 && weight > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
