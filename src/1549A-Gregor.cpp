a6f94da2c9f39e88b7747bf680f9c0e7712ab1bc
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-09 19:48:51 +0100

    solution 1549A

---
+++ src/1549A-Gregor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int iter;
+    cin >> iter;
+    for (int i = 0; i < iter; i++)
+    {
+        int n;
+        cin >> n;
+
+        cout << 2 << " " << n - 1 << "\n";
+    }
+}
