3f93029bf4e360a983a882cbe3810956f603d258
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-10 17:41:05 +0100

    1525A solution

---
+++ src/1525A-PotionMaking.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int iter;
+    cin >> iter;
+    for (int i = 0; i < iter; i++)
+    {
+        int n;
+        cin >> n;
+
+        cout << 100 / __gcd(n, 100) << "\n";
+    }
+}
