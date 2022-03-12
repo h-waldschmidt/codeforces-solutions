6fb17a33f2e4885f2b1dd9af170987679031ec15
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-12 10:48:40 +0100

    1482A solution

---
+++ src/1482A-PrisonBreak.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int iter;
+    cin >> iter;
+    for (int i = 0; i < iter; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a * b << "\n";
+    }
+}
