6997a7915b82c899822207ea84a66bc50cbfcdc8
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-04-25 16:37:30 +0200

    solution for the famous 2Sum problem

---
+++ leetcode/src/2Sum.cpp
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+  public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> answer;
+        unordered_map<int, int> lookUp;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+            if (lookUp.find(complement) != lookUp.end())
+            {
+                answer = {i, lookUp[complement]};
+                break;
+            }
+            else
+            {
+                lookUp.insert(make_pair(nums[i], i));
+            }
+        }
+        return answer;
+    }
+};
